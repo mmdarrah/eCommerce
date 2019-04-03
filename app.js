8dee80efed5bdb1102e5ccd2c089c3b77db94a14
Mohammed Darrah <mmdarrah@hotmail.com>
2019-04-03 21:08:41 +0200

    test2

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
@@ -9,8 +7,7 @@ xhttp.onreadystatechange = function() {
     let output = "";
 
     for (let i = 0; i < items.length; i++) {
-      output +=
-       ` <div class='col-md-4'  >
+      output += ` <div class='col-md-4'  >
         <IMG src= 
         ${items[i].image} class="card-img-top" 
         > 
@@ -24,15 +21,33 @@ xhttp.onreadystatechange = function() {
         </div>`;
     }
 
-    document.getElementById("list").innerHTML = output;
+    let list = document.getElementById("list");
+    if (list != null) {
+      list.innerHTML = output;
+    }
+
+    let cart = localStorage.getItem("basket");
+    if (cart != null) {
+      cart = JSON.parse(cart);
+      let basketOutput = "";
+      for (let i = 0; i < cart.length; i++) {
+        basketOutput += ` <div class='col-md-4'  >
+>
+        <h5 class="card-title" >${cart[i].name}</h5>
+        
+        <p class="card-text"><h5>${cart[i].price}</h5></p>
+        </div>
+        </div>`;
+      }
+      let basketList = document.getElementById("basket");
+      if (basketList != null) {
+        basketList.innerHTML = basketOutput;
+      }
+    }
   }
 };
 xhttp.open("GET", "data.json", true);
 xhttp.send();
-
-
-
-
 
 /* (function(){
   const cartInfo = document.getElementById('cart-info');
@@ -44,27 +59,29 @@ xhttp.send();
 })();
  */
 
-
-
-
 /* jQuery start here */
-$(document).ready(function(){
+$(document).ready(function() {
+  $("button").on("click", function() {
+    let parent = $(this).siblings("h5");
+    console.log("Selected Product");
+    console.log(parent[0].innerText);
+    console.log(parent[1].innerText);
+    let temp = localStorage.getItem("basket");
+    if (temp != null) {
+      let cart = JSON.parse(temp);
 
-  
-    
-    $("button").on("click",  function() {
-      let $name = $(this).find(name);
-      console.log($name);
-        
-    });
-
-   
-    
-    
-
-
-
-
-});//Ready
-
-
+      cart.push({ name: parent[0].innerText, price: parent[1].innerText });
+      cart = JSON.stringify(cart);
+      localStorage.setItem("basket", cart);
+    } else {
+      let buyBasket = [];
+      buyBasket.push({
+        name: parent[0].innerText,
+        price: parent[1].innerText
+      });
+      let cart = JSON.stringify(buyBasket);
+      localStorage.setItem("basket", cart);
+    }
+    console.log(buyBasket);
+  });
+}); //Ready
