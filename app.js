7228b4e9cadb3ada5ce0fb37cbf8bf574be6091d
Mohammed Darrah <mmdarrah@hotmail.com>
2019-04-05 12:32:35 +0200

    id quantity testing

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-
+/*Ajax request för att hämta data från JSON fil */
 
 
 let xhttp = new XMLHttpRequest();
@@ -31,37 +31,18 @@ xhttp.onreadystatechange = function () {
 
     }
 
-    /* function loadItems() {
-      let allItems = [];
-      for (let i = 0; i < items.length; i++) {
-        allItems.push({
-          id: items[i].id,
-          name: items[i].name,
-          image: items[i].image,
-          price: items[i].price,
-
-        })
-
-      }
-
-      return allItems
+          let list = document.getElementById("list");
+          if (list != null) {
+            list.innerHTML = output;
+          }
 
 
-    }
-     console.log(loadItems());  */
-
-
-    let list = document.getElementById("list");
-    if (list != null) {
-      list.innerHTML = output;
-    }
-
-    let cart = localStorage.getItem("basket");
-    if (cart != null) {
-      cart = JSON.parse(cart);
-      let basketOutput = "";
-      for (let i = 0; i < cart.length; i++) {
-        basketOutput += ` 
+          let cart = localStorage.getItem("basket");
+          if (cart != null) {
+            cart = JSON.parse(cart);
+            let basketOutput = "";
+            for (let i = 0; i < cart.length; i++) {
+              basketOutput += ` 
                     
               <tr>
                 <td><IMG style="width:80px;hight:auto;"src=
@@ -71,10 +52,11 @@ xhttp.onreadystatechange = function () {
                 <td><input  id="num" type="number" name="quantity" min="1" max="10"></input></td>
                 <td><span><i class="fas fa-trash-alt"></i></span></td>
               </tr>
-       
-        
-        `;
+               `;
       }
+
+
+
       let delBtn = document.getElementById("delBtn");
       let basketList = document.getElementById("basket");
       if (basketList != null) {
@@ -88,49 +70,34 @@ xhttp.open("GET", "data.json", true);
 xhttp.send();
 
 
-/* let quantity = $("input").val()
-    console.log(quantity); */
+
+
+
+
+
+
+
 
 /* jQuery start here */
-/* $(document.body).on('click', '.add', function () {
-        const cardElem = $(this).closest('.card'),
-            id = parseInt(cardElem.data('id')),
-            quantity = parseInt(cardElem.find('.quantity').val());
 
-        addToCart(id, quantity);
-        fillCart();
-    }); */
 $(document).ready(function () {
-
-  /* $("button").on('click', '.num', function () {
-    let cardElem = $(this).closest('input')
-    
-      quantity = parseInt(cardElem.find('.quantity').val());
-    console.log(quantity);
-
-  }) */
-
-
-
-  /* $("button").on('click', function () {
-    let quantity = $(".add").val()
-   console.log(quantity);
- })  */
-
 
 
 
   $("button#add").on("click", function () {
 
     let parent = $(this).siblings("h5");
+
     let image = $(this).parents().find("#img").children("img");
     let imageSource = image[0].currentSrc;
-    console.log(imageSource);
+    /* console.log(imageSource); */
 
     let allParents = $(this).siblings();
     let inputValue = allParents[4].value;
-    //TODO
-    //here you shoud check if the input value is undifined then you set 1
+
+    if (inputValue == 0 || inputValue == "") {
+      inputValue = 1
+    }
 
     /* console.log(parent); */
     /* console.log(parent[2].innerHTML); */
@@ -148,24 +115,34 @@ $(document).ready(function () {
 
 
       let cart = JSON.parse(cartFromLocalStorage);
+      /* console.log(cart); */
+
+      /* cart.forEach(id => {
+        if(id == null)
+
+
+      }); */
       //TODO
       //here you need to check if the id exist in the list
 
-      //if exists then get the item there should be a function in jave script that removes an item
+      //if exists then get the item there should be a function in javescript that removes an item
       //from a list and then you should calculate the quanity
-      
-      
+
+
       cart.push({
         name: parent[0].innerText,
         id: parent[1].innerText,
         price: parent[2].innerText,
         image: imageSource,
         quantity: inputValue
-        /* quantity: */
+        
       });
+
+
       cart = JSON.stringify(cart);
       localStorage.setItem("basket", cart);
-    } else {
+          } 
+          else {
       // if the local storage does not contain a cart
 
       //add the item directly
@@ -185,7 +162,10 @@ $(document).ready(function () {
 
 
 
-  /* remove items from lista */
+
+
+
+  /* remove all items from basket */
 
   $(".table").on("click", "span", function () {
 
@@ -203,7 +183,7 @@ $(document).ready(function () {
   });
 
 
-  /* Total price */
+
 
 
   /* Buy btn */
@@ -216,4 +196,52 @@ $(document).ready(function () {
     location.reload();
   })
 
+
+
+  /* Total price */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }); //Ready
+
+
+
+
+/* function loadItems() {
+     let allItems = [];
+     for (let i = 0; i < items.length; i++) {
+       allItems.push({
+         id: items[i].id,
+         name: items[i].name,
+         image: items[i].image,
+         price: items[i].price,
+
+       })
+
+     }
+
+     return allItems
+
+
+   }
+    console.log(loadItems());  */
