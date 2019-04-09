a871614371f7f47e03e1cff1095c2f1b8775b728
Mohammed Darrah <mmdarrah@hotmail.com>
2019-04-09 11:33:20 +0200

    bug fixed

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-/*Ajax request för att hämta data från JSON fil */
+
 
 
 let xhttp = new XMLHttpRequest();
@@ -21,8 +21,8 @@ xhttp.onreadystatechange = function () {
         <h5 class="card-title" >${items[i].name}</h5>
         <p class="card-text">Diameter: ${items[i].height}</p>
         <p class="card-text">Ursprung: ${items[i].origin}</p>
-        <h4 style="display: none;">${
-        items[i].id}</h4><input  id="num" type="number" name="quantity" min="1" max="10" value="1"></input>
+        <h5 style="display: none;">${
+        items[i].id}</h5><input  id="num" type="number" name="quantity" min="1" max="10" value="1"></input>
         <p class="card-text"><h5>${items[i].price} Kr</h5></p>
         <button type="button" id="add" class="btn btn-success btn-lg btn-block btn-sm">Köpa</button>
         </div>
@@ -31,11 +31,13 @@ xhttp.onreadystatechange = function () {
 
     }
 
+    
+
+
     let list = document.getElementById("list");
     if (list != null) {
       list.innerHTML = output;
     }
-
 
     let cart = localStorage.getItem("basket");
     if (cart != null) {
@@ -44,19 +46,19 @@ xhttp.onreadystatechange = function () {
       for (let i = 0; i < cart.length; i++) {
         basketOutput += ` 
                     
-              <tr >
+              <tr>
                 <td><IMG style="width:80px;hight:auto;"src=
                   ${cart[i].image} class="card-img-top"></td>
                 <td>${cart[i].name}</td>
-                <td>${cart[i].price} kr</td> 
+                <td>${cart[i].price}</td> 
                 <td><input  id="num" type="number" name="quantity" min="1" max="10"></input></td>
-                <td><a id="${cart[i].id}"href="#"><span><i class="fas fa-trash-alt"></i></span></a></td>
-                </tr>
-                `;
-
-        /*  <td><a onclick="deleteItem(id) "href="#"><span><i class="fas fa-trash-alt"></i></span></a></td> */
-
+                <td> <td><a id="${cart[i].id}"href="#"><span><i class="fas fa-trash-alt"></i></span></a></td></td>
+              </tr>
+       
+        
+        `;
       }
+
 
       /* Total price start*/
       if (list == null) {
@@ -86,10 +88,7 @@ xhttp.onreadystatechange = function () {
           totalPrice.innerHTML = totResult;
         }
       }
-      /* Total price end */
-
-
-
+            /* Total price end */
 
 
 
@@ -106,53 +105,34 @@ xhttp.open("GET", "data.json", true);
 xhttp.send();
 
 
-/* Delete clicked item */
-
-/* function deleteItem(id){
-  console.log(id);
-  let cart = localStorage.getItem("basket");
-  cart = JSON.parse(cart);
-   for (let i = 0; i < cart.length; i++) { 
-    
-    
-     console.log(cart[i].id); 
-
-
-      if (cart[i].id === id){
-      console.log("test");
-      cart.splice(i, 1)
-       cart = JSON.stringify(cart);
-       localStorage.setItem("basket", cart);
-
-    }
-  }
-} */
-
-
-
-/* jQuery start here */
-
 $(document).ready(function () {
+
+  
+
 
 
 
   $("button#add").on("click", function () {
 
     let parent = $(this).siblings("h5");
-    let btnId = $(this).siblings("h4");
-    let clickId = btnId[0].innerText;
-    /* console.log(clickId); */
-
     let image = $(this).parents().find("#img").children("img");
     let imageSource = image[0].currentSrc;
-    /* console.log(imageSource); */
+    console.log(imageSource);
 
+
+
+
+
+
+    
     let allParents = $(this).siblings();
     let inputValue = allParents[4].value;
 
     if (inputValue == 0 || inputValue == "") {
       inputValue = 1
     }
+    //TODO
+    //here you shoud check if the input value is undifined then you set 1
 
     /* console.log(parent); */
     /* console.log(parent[2].innerHTML); */
@@ -162,61 +142,20 @@ $(document).ready(function () {
 
     let cartFromLocalStorage = localStorage.getItem("basket");
 
+
     if (cartFromLocalStorage != null) {
 
       // if we already have the cart in the local storage
 
+
+
       let cart = JSON.parse(cartFromLocalStorage);
-
-      /* console.log(cart); */
-
-      for (let i = 0; i < cart.length; i++) {
-        /* console.log(cart[i].id); */
-        if (cart[i].id == clickId) {
-          console.log("test");
-        }
-
-
-      }
-
-
-
-
-
-
-
       //TODO
       //here you need to check if the id exist in the list
 
-      //if exists then get the item there should be a function in javescript that removes an item
+      //if exists then get the item there should be a function in jave script that removes an item
       //from a list and then you should calculate the quanity
 
-      /*  for (let i = 0; i < cart.length; i++) {
-         if (cart[i].id == null) {
- 
- 
-           cart.push({
-             name: parent[0].innerText,
-             id: parent[1].innerText,
-             price: parent[2].innerText,
-             image: imageSource,
-             quantity: inputValue
- 
-           });
- 
-        } else if (cart[i].id === parent[1].innerText) {
- 
-          console.log("test");
- 
- 
-          let newQuan = (cart[i].quantity += cart.inputValue);
- 
-          console.log(newQuan);
-           
-         }
-         
-       }
-  */
 
       cart.push({
         name: parent[0].innerText,
@@ -224,15 +163,11 @@ $(document).ready(function () {
         price: parent[2].innerText,
         image: imageSource,
         quantity: inputValue
-
+        /* quantity: */
       });
-
-
       cart = JSON.stringify(cart);
       localStorage.setItem("basket", cart);
-
-    }
-    else {
+    } else {
       // if the local storage does not contain a cart
 
       //add the item directly
@@ -252,11 +187,8 @@ $(document).ready(function () {
 
 
 
+  /* remove items from lista */
 
-
-
-
-  /* remove clicked item from basket */
   $(".table").on("click", "span", function () {
 
     let removItem = $(this).parents('tr').fadeOut(500, function () {
@@ -265,10 +197,6 @@ $(document).ready(function () {
     });
   })
 
-
-
-
-  /* remove all items from basket */
   $('.removeAll').on('click', function () {
     localStorage.removeItem('basket');
 
@@ -277,7 +205,7 @@ $(document).ready(function () {
   });
 
 
-
+  /* Total price */
 
 
   /* Buy btn */
@@ -290,52 +218,4 @@ $(document).ready(function () {
     location.reload();
   })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }); //Ready
-
-
-
-
-/* function loadItems() {
-     let allItems = [];
-     for (let i = 0; i < items.length; i++) {
-       allItems.push({
-         id: items[i].id,
-         name: items[i].name,
-         image: items[i].image,
-         price: items[i].price,
-
-       })
-
-     }
-
-     return allItems
-
-
-   }
-    console.log(loadItems());  */
