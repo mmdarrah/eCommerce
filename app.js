f0e021cc93e2349af6f450279111e0ccc00fd046
Mohammed Darrah <mmdarrah@hotmail.com>
2019-04-09 21:38:01 +0200

    after fixing the bug

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ xhttp.onreadystatechange = function () {
     for (let i = 0; i < items.length; i++) {
       output += ` <div id="img"  class='col-md-4'   >
         <IMG  src= 
-        ${items[i].image} style="width:250px;hight:auto; class="card-img-top" >
+        ${items[i].image} style="width:200px;hight:auto; class="card-img-top" >
         <div class="card-body"  >
         <h5 class="card-title" >${items[i].name}</h5>
         <p class="card-text">Diameter: ${items[i].height}</p>
@@ -31,7 +31,7 @@ xhttp.onreadystatechange = function () {
 
     }
 
-    
+
 
 
     let list = document.getElementById("list");
@@ -50,9 +50,9 @@ xhttp.onreadystatechange = function () {
                 <td><IMG style="width:80px;hight:auto;"src=
                   ${cart[i].image} class="card-img-top"></td>
                 <td>${cart[i].name}</td>
-                <td>${cart[i].price} Kr</td> 
+                <td>${cart[i].price * cart[i].quantity} Kr</td> 
                 <td>${cart[i].quantity} st</td> 
-                <td><input  id="${cart[i].id}" type="number" name="quantity" min="1" max="10" value="1"></input></td>
+                <td><input  id="${cart[i].id}" type="number" name="quantity" min="1" max="10" value="${cart[i].quantity}"></input></td>
                 <td> <a "href="#"><span><i class="fas fa-trash-alt"></i></span></a></td>
               </tr>
        
@@ -63,7 +63,7 @@ xhttp.onreadystatechange = function () {
 
       /* Total price start*/
 
-      function totalPrice () {
+      function totalPrice() {
         if (list == null) {
           let cartPrice = localStorage.getItem("basket");
           if (cartPrice != null) {
@@ -72,7 +72,7 @@ xhttp.onreadystatechange = function () {
 
             for (let i = 0; i < cart.length; i++) {
               /* console.log(cart[i].price); */
-              total.push(cart[i].price);
+              total.push((cart[i].price * cart[i].quantity));
             }
             /* console.log(total); */
 
@@ -93,8 +93,8 @@ xhttp.onreadystatechange = function () {
         }
 
       }
-      
-            /* Total price end */
+
+      /* Total price end */
       totalPrice()
 
 
@@ -113,7 +113,7 @@ xhttp.send();
 
 $(document).ready(function () {
 
-  
+
 
 
 
@@ -122,7 +122,7 @@ $(document).ready(function () {
 
     let parent = $(this).siblings("h5");
     let image = $(this).parents().find("#img").children("img");
-   /*  console.log(image); */
+    /*  console.log(image); */
     let imageSource = image[0].currentSrc;
     /* console.log(imageSource); */
     let allParents = $(this).siblings();
@@ -133,7 +133,7 @@ $(document).ready(function () {
     let btnId = $(this).siblings("h5");
     /* console.log(btnId); */
     let clickId = allParents[3].innerText;
-        /* console.log(clickId); */
+    /* console.log(clickId); */
 
 
 
@@ -169,49 +169,55 @@ $(document).ready(function () {
 
       //if exists then get the item there should be a function in jave script that removes an item
       //from a list and then you should calculate the quanity
-
+      let ItemExists = false;
+      let curentIndex = 0;
       for (let i = 0; i < cart.length; i++) {
         /* console.log(cart[i].id); */
         if (cart[i].id == clickId) {
           /* console.log("test"); */
-
-          let newQua = +cart[i].quantity + +inputValue
-          let newPrs = cart[i].price * newQua
-          
-          cart.splice(i, 1); 
-
-          /* console.log(newPrs); */
-
-          /* this.quantity = newQua */
-          
-          cart.push({
-            
-            name: parent[0].innerText,
-            id: parent[1].innerText,
-            price: newPrs,
-            image: imageSource,
-            quantity: newQua
-
-          });
-         
-         cart = JSON.stringify(cart);
-         localStorage.setItem("basket", cart);
-
+          ItemExists = true;
+          curentIndex = i;
+          break;
 
         }
+      }
+
+      if (ItemExists) {
+
+        let newQua = +cart[curentIndex].quantity + +inputValue
+        let newPrice = cart[curentIndex].price;
+
+        cart.splice(curentIndex, 1);
+
+        /* console.log(newPrs); */
+
+        /* this.quantity = newQua */
+
+        cart.push({
+
+          name: parent[0].innerText,
+          id: parent[1].innerText,
+          price: newPrice,
+          image: imageSource,
+          quantity: newQua
+
+        });
 
 
+
+      } else {
+
+        cart.push({
+          name: parent[0].innerText,
+          id: parent[1].innerText,
+          price: parent[2].innerText,
+          image: imageSource,
+          quantity: inputValue
+
+        });
       }
 
 
-      cart.push({
-        name: parent[0].innerText,
-        id: parent[1].innerText,
-        price: parent[2].innerText,
-        image: imageSource,
-        quantity: inputValue
-        
-      });
       cart = JSON.stringify(cart);
       localStorage.setItem("basket", cart);
     } else {
@@ -239,7 +245,7 @@ $(document).ready(function () {
   $(".table").on("click", "span", function () {
     let cartFromLocalStorage = localStorage.getItem("basket");
     let cart = JSON.parse(cartFromLocalStorage);
-    
+
     let xItem = $(this).parents('tr').fadeOut(500, function () {
       /* I need the ID */
 
@@ -255,17 +261,18 @@ $(document).ready(function () {
 
 
 
-        }}
+        }
+      }
       cart = JSON.stringify(cart);
       localStorage.setItem("basket", cart);
       location.reload();
 
-          /* console.log(removItemId); */
+      /* console.log(removItemId); */
       /* xx = removItem.childNodes.id */
       /* removId = removItem[0].closest("id")
       console.log(removId); */
       /* $(this).remove(); */
-      
+
     });
   })
 
@@ -277,7 +284,7 @@ $(document).ready(function () {
   });
 
 
-  
+
 
 
   /* Buy btn */
@@ -290,50 +297,115 @@ $(document).ready(function () {
     location.reload();
   })
 
-  
-  
-  $(".num").on("change",function(){
-    let $input = $(this).val()
-    /* console.log($input); */
 
 
-    let cartFromLocalStorage = localStorage.getItem("basket");
-    let cart = JSON.parse(cartFromLocalStorage);
-    let xId = $(this).children().find("input");
-    let clickId = xId[0].id;
+  $(".num").on("change", function () {
+
+    // let $input = $(this).val()
+    // console.log($input);
+    let $input = $(this).children().find("input");
+    let clickId = $input[0].id;
+    let inputVal = $input[0].value;
     console.log(clickId);
+    let cartFromLocalStorage = localStorage.getItem("basket");
 
-    if (inputValue == 0 || inputValue == "") {
-      inputValue = 1
-    }
+    let cart = JSON.parse(cartFromLocalStorage);
+    //TODO
+    //here you need to check if the id exist in the list
 
+    //if exists then get the item there should be a function in jave script that removes an item
+    //from a list and then you should calculate the quanity
+    let ItemExists = false;
+    let curentIndex = 0;
     for (let i = 0; i < cart.length; i++) {
-      
+      /* console.log(cart[i].id); */
       if (cart[i].id == clickId) {
-    let newQua = +cart[i].quantity + +$input
-    let newPrs = cart[i].price * newQua
-
-    cart.splice(i, 1);
-
-    /* console.log(newPrs); */
-
-    /* this.quantity = newQua */
-
-    cart.push({
-
-      name: parent[0].innerText,
-      id: parent[1].innerText,
-      price: newPrs,
-      image: imageSource,
-      quantity: newQua
-
-    });
-
-    cart = JSON.stringify(cart);
-    localStorage.setItem("basket", cart);
+        /* console.log("test"); */
+        ItemExists = true;
+        curentIndex = i;
+        break;
 
       }
     }
+
+    if (ItemExists) {
+
+      let newQua = inputVal
+      let newPrice = cart[curentIndex].price;
+      let newname = cart[curentIndex].name;
+      let newimage = cart[curentIndex].image;
+
+      cart.splice(curentIndex, 1);
+      cart.push({
+
+        name: newname,
+        id: clickId,
+        price: newPrice,
+        image: newimage,
+        quantity: newQua
+
+      });
+
+      cart = JSON.stringify(cart);
+      localStorage.setItem("basket", cart);
+      location.reload();
+
+
+      /* console.log(newPrs); */
+
+      /* this.quantity = newQua */
+
+      // cart.push({
+
+      //   name: parent[0].innerText,
+      //   id: parent[1].innerText,
+      //   price: newPrice,
+      //   image: imageSource,
+      //   quantity: newQua
+
+      // });
+
+
+
+    }
+
+    // let cartFromLocalStorage = localStorage.getItem("basket");
+    // let cart = JSON.parse(cartFromLocalStorage);
+    // let xId = $(this).children().find("input");
+    // let clickId = xId[0].id;
+    // console.log(clickId);
+
+    // if (inputValue == 0 || inputValue == "") {
+    //   inputValue = 1
+    // }
+
+    // for (let i = 0; i < cart.length; i++) {
+
+    //   if (cart[i].id == clickId) {
+    //     let newQua = +cart[i].quantity + +$input
+    //     let newPrs = cart[i].price * newQua
+
+    //     cart.splice(i, 1);
+
+    //     /* console.log(newPrs); */
+
+    //     /* this.quantity = newQua */
+
+    //     cart.push({
+
+    //       name: parent[0].innerText,
+    //       id: parent[1].innerText,
+    //       price: newPrs,
+    //       image: imageSource,
+    //       quantity: newQua
+
+    //     });
+
+    //     cart = JSON.stringify(cart);
+    //     localStorage.setItem("basket", cart);
+
+    //}
+    //  }
   })
 
 
